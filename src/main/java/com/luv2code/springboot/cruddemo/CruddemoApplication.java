5c12b52be7fa9aef6dba88e40af02d0af657579a
vilas639 <vilasjdhv639@gmail.com>
2020-05-31 15:55:29 +0530

    change aws service

--- src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -2,11 +2,20 @@ package com.luv2code.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class CruddemoApplication {
+@ComponentScan(basePackages="com.luv2code.springboot")
+public class CruddemoApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
+	
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(CruddemoApplication.class);
+	  }
 }
